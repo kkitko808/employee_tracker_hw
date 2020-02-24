de3e069aebdd158537cf7c80cc42d71595a63cc3
kkitko808 <dento85@tutamail.com>
2020-02-24 13:23:39 -0600

    have database with entries into the tables

---
+++ schema.sql
@@ -0,0 +1,36 @@
+DROP DATABASE IF EXISTS employee_tracker_db;
+CREATE database employee_tracker_db;
+
+USE employee_tracker_db;
+
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30)NOT NULL ,
+    role_id INT NOT NULL,
+    manager_id INT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE role (
+    id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL(10,4) NOT NULL,
+    department_id INT NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE department (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(30) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Kalani","K", 1, 2), ("Brittni","L", 2, 3), ("Tara","H", 3 );
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("Salesperson","100000",1), ("Lead Engineer","200000", 2), ("150000","H", 3);
+
+INSERT INTO department (name)
+VALUES ("Sales"), ("Engineering"),("Legal");
